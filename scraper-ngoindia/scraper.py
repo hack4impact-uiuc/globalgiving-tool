08ced4da88058dda5df1efbd60e1138b209e0736
Pavani Malli <pavanim@wirelessprv-10-193-51-218.near.illinois.edu>
2018-10-06 18:09:25 -0500

    get each NGO data pseudocode

--- scraper-ngoindia/scraper.py
+++ scraper-ngoindia/scraper.py
@@ -5,12 +5,20 @@ def get_page_data():
     # Specify url to scrape from
     target_url = requests.get("https://ngosindia.com/ngos-of-india/")
     page_data = BeautifulSoup(target_url.content, "html.parser")
-    return page_data.body.contents
+    state_list = page_data.find_all(attrs={'class': 'textwidget'})[1].find_all('a')
+    #go through each state
+    #for i in state_list:
+        #extract link, get page_data of this url
 
-def get_specific_page_data():
+    #go through each county
+    #go through each NGO (call get_specific_page_data) for each NGO
+
+def get_specific_page_data(url):
+    #replace with url
     target_url = requests.get("https://andaman-nicobar.ngosindia.com/fire-gospel-mission-south-andaman-2/")
     page_data = BeautifulSoup(target_url.content, "html.parser")
-    return page_data.find_all(attrs={'class': 'ngo-postcontent'})
-    #return page_data.body.contents
-
-#paragraph data: ngo-postcontent clearfix
+    ngo_name = page_data.find_all(attrs={'class': 'ngo-postheader'})
+    contents = page_data.find_all(attrs={'class': 'ngo-postcontent'})
+    contents[0] = ngo_name
+    #contents includes data regarding NGO name, address, contact information, & mission statement
+    return contents
