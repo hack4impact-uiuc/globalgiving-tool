570243ce5f37a1923e7eada1fc18c9509c5aec88
Aryn Harmon <achcello@gmail.com>
2018-10-06 18:47:38 -0500

    scraper can now get all links to NGOs and weed out duplicates based on link

--- microservices/scraper-hati-malaysia/scraper.py
+++ microservices/scraper-hati-malaysia/scraper.py
@@ -12,17 +12,30 @@ def get_page_data():
         if linkText is not None:
             if 'category' in linkText:
                 catLinks.append(linkText)  # get the link for the category
+    print('retreived category page links')
 
     ngoLinks = []  # build list of links for NGOs
     for link in catLinks:
         categoryName = link[28:-1]
-        print(categoryName)
         target_url = requests.get(link)
         page_data = BeautifulSoup(target_url.content, "html.parser")
-        ngoContent = page_data.find_all('a')
+        ngoContent = page_data.body.find_all('a', {'class': 'read-more'})  # we can grab the ngo links most readily from the read more buttons
         for ngoLink in ngoContent:
-            ngoLinks.append(ngoLink)
-            #if ngoLink is not None and categoryName in ngoLink:
-            #    ngoLinks.append(ngoLink)
+            ngoLinks.append(ngoLink.get('href'))
+            if categoryName in ngoLink:
+                ngoLinks.append(ngoLink)
+    print('retreived NGO page links')
+    
+    # get rid of duplicate links
+    ngoLinks = list(set(ngoLinks))
+    print('purged duplicate links')
+
+    # now scrape relevant information from the individual NGOs
+    for ngoLink in ngoLinks:
+        target_url = requests.get(ngoLink)
+        page_data = BeautifulSoup(target_url.content, "html.parser")
+        
+        break
+
 
     return ngoLinks
