b89c2fda7ef7766f6566701100baefd1d3a3756d
alan-fang <fang.alan.7@gmail.com>
2018-10-13 18:50:36 -0500

    add the correct updated files

--- scraper-vietgate/scraper.py
+++ scraper-vietgate/scraper.py
@@ -1,21 +1,23 @@
 from bs4 import BeautifulSoup
 from requests import get
+from org import create_org
 
-url = 'https://www.viet.net/community/nonprofit/'
+url = "https://www.viet.net/community/nonprofit/"
+
 
 def get_page_data():
     response = get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    print(len(soup.findAll('p')))
-    for link in soup.findAll('p'):
+    soup = BeautifulSoup(response.text, "html.parser")
+    for link in soup.findAll("p"):
         parse_data(link.text)
+
 
 def parse_data(link):
     if link == None:
         return
-    for line in link.split('\n\n'):
-        if len(line) > 0:
-            print(list(filter(None, line.splitlines())))
+    for line in link.split("\n\n"):
+        create_org(line.strip().split("\n"))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     get_page_data()
