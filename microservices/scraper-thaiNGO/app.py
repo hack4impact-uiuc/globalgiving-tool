941ed7072f29a557fb995bbc68f6184f01b80f26
Skyler Shi <skylershi@wirelessprv-10-194-14-235.near.illinois.edu>
2018-10-13 18:12:36 -0500

    Scraper black reformatted

--- microservices/scraper-thaiNGO/app.py
+++ microservices/scraper-thaiNGO/app.py
@@ -3,10 +3,12 @@ from scraper import basepage_scrape
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def my_first_route():
     basepage_scrape()
     return str(basepage_scrape())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
--- microservices/scraper-thaiNGO/scraper.py
+++ microservices/scraper-thaiNGO/scraper.py
@@ -18,21 +18,31 @@ ngos = [{
 }]
 """
 ngos = []
-ngos_store_keys = ["ngo_name", "ngo_URL", "phone_num", "email", "registration_id", "year_established", "description"]
+ngos_store_keys = [
+    "ngo_name",
+    "ngo_URL",
+    "phone_num",
+    "email",
+    "registration_id",
+    "year_established",
+    "description",
+]
 
 """
 DESCRIPTION: scrapes the base directory for links to specific Thai NGO wiki pages        
 """
+
+
 def basepage_scrape():
     page = requests.get(baseurl)
-    soup = BeautifulSoup(page.content, 'html5lib')
-    #find all NGO entries represented as a row in html table
-    ngo_tr_tags = soup.find_all('tr')
+    soup = BeautifulSoup(page.content, "html5lib")
+    # find all NGO entries represented as a row in html table
+    ngo_tr_tags = soup.find_all("tr")
 
     for row in ngo_tr_tags:
-        ngo_info = row.find_all('td')
-        #dictionary to store ngo information
-        ngo={}
+        ngo_info = row.find_all("td")
+        # dictionary to store ngo information
+        ngo = {}
         if ngo_info is not None and len(ngo_info) > 0:
             ngo_name = ngo_info[1]
             ngo["name"] = ngo_name.text.lstrip()
@@ -40,20 +50,20 @@ def basepage_scrape():
             ngo["description"] = ngo_descr.text.lstrip()
             ngo["URL"] = ""
             if ngo_info[3].a is not None:
-                ngo_URL = ngo_info[3].a.get('href')
+                ngo_URL = ngo_info[3].a.get("href")
                 ngo["URL"] = ngo_URL
-            #filter invalid email address formats
+            # filter invalid email address formats
             ngo["email"] = ""
             if len(ngo_info[4].text) > 5 and "requested" not in ngo_info[4].text:
-                #strip all left spaces, replace @ symbol into email
+                # strip all left spaces, replace @ symbol into email
                 ngo_email = ngo_info[4].text.lstrip().replace(" at ", "@")
                 ngo["email"] = ngo_email
-        #append ngo dictionary to list of ngos
+        # append ngo dictionary to list of ngos
         ngos.append(ngo)
-    
-    #write to csv to check output
+
+    # write to csv to check output
     keys = ["name", "description", "URL", "email"]
-    with open('thai_ngos.csv', 'w') as w:
+    with open("thai_ngos.csv", "w") as w:
         dict_writer = csv.DictWriter(w, keys)
         dict_writer.writeheader()
         dict_writer.writerows(ngos)
