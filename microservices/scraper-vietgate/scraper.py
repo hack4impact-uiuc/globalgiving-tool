3ab2546be0174795a5c7e6257b20b1fdbe2a872d
alan-fang <fang.alan.7@gmail.com>
2018-10-13 18:03:09 -0500

    finish basic parser for VietGate

--- microservices/scraper-vietgate/scraper.py
+++ microservices/scraper-vietgate/scraper.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
 from requests import get
+from org import create_org
 
 url = 'https://www.viet.net/community/nonprofit/'
 
 def get_page_data():
     response = get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    print(len(soup.findAll('p')))
+
     for link in soup.findAll('p'):
         parse_data(link.text)
 
@@ -14,8 +15,7 @@ def parse_data(link):
     if link == None:
         return
     for line in link.split('\n\n'):
-        if len(line) > 0:
-            print(list(filter(None, line.splitlines())))
+        create_org(line.strip().split('\n'))
 
 if __name__ == '__main__':
     get_page_data()
