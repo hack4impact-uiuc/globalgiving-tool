3738be1a2bd42f98431e83831c479ce4623f67b6
alan-fang <fang.alan.7@gmail.com>
2018-10-13 18:43:04 -0500

    finish basic functionality for the VietGate scraper

---
+++ scraper-vietgate/org.py
@@ -0,0 +1,7 @@
+class Org:
+    def __init__(self, name, phone, email, address, contact):
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.address = address
+        self.contact = contact
