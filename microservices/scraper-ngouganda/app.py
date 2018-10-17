625c3a3798a657d12ab824add348e1db24591070
Alan Ren <alanren2@illinois.edu>
2018-10-17 18:07:34 -0500

    fixed comments

--- microservices/scraper-ngouganda/app.py
+++ microservices/scraper-ngouganda/app.py
@@ -1,15 +1,16 @@
 from flask import Flask
 from scraper import get_page_data
+
 app = Flask(__name__)
 
-@app.route('/')
-def my_first_route():
-	# print(page_data())
-	return page_data()
+@app.route("/routes")
+def get_routes():
+    return ["/data"]
 
-@app.route('/data')
+@app.route("/data")
 def page_data():
-	return str(get_page_data())
+    return str(get_page_data())
 
-if __name__ == '__main__':
-	app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
