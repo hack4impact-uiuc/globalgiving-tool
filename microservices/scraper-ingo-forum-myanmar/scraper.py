27d109ebf5538983509d4c288784b52aa1ab1092
alan-fang <fang.alan.7@gmail.com>
2018-10-17 18:11:12 -0500

    basic scraper implementation

---
+++ microservices/scraper-ingo-forum-myanmar/scraper.py
@@ -0,0 +1,25 @@
+from bs4 import BeautifulSoup
+from requests import get
+from org import create_org_ingo_forum_myanmar
+
+url = "http://www.ingoforummyanmar.org/en/members"
+
+
+def get_page_data():
+    target_url = get(url)
+    page_data = BeautifulSoup(target_url.content, "html.parser")
+
+    tags = page_data.find_all("div", attrs={"class": ["member-info"]})
+    for tag in tags:
+        name = tag.find("div", attrs={"class": ["member-title"]})
+        address = tag.find("div", attrs={"class": ["member-address"]})
+        phone = tag.find("div", attrs={"class": ["member-phone"]})
+        email = tag.find("div", attrs={"class": ["member-email"]})
+        org_url = tag.find("div", attrs={"class": ["web-link"]})
+
+        create_org_ingo_forum_myanmar(name, address, phone, email, org_url)
+        print()
+
+
+if __name__ == "__main__":
+    get_page_data()
