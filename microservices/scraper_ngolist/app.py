a273b06e0394a32a567e96e0b1522930941d74f3
Skyler Shi <skylershi@wirelessprv-10-194-14-235.near.illinois.edu>
2018-10-24 14:50:08 -0500

    Scrape ngo_list almost done, needs work on formating edge case

--- microservices/scraper_ngolist/app.py
+++ microservices/scraper_ngolist/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from scraper import basepage_scrape
+from scraper import country_page_scrape
 
 app = Flask(__name__)
 
@@ -9,9 +10,14 @@ def scrape_all_ngos():
     return str(basepage_scrape())
 
 
+@app.route("/colombia")
+def scrape_colombia():
+    return str(country_page_scrape("/colombia.html"))
+
+
 @app.route("/routes")
 def return_all_routes():
-    return ["/scrape_all"]
+    return ["/scrape_all", ]
 
 
 if __name__ == "__main__":
--- microservices/scraper_ngolist/scraper.py
+++ microservices/scraper_ngolist/scraper.py
@@ -3,7 +3,7 @@ import requests
 from urllib.request import urlopen
 import csv
 
-baseurl = "https://www.thengolist.com/"
+base_url = "https://www.thengolist.com"
 
 
 # list of dictionaries to store ngo information
@@ -20,28 +20,110 @@ ngos_store_keys = [
     "facebook"
 ]
 
+#dictionary of key:value pairs of "region": [list of country links]
+region_country_links_dict = {}
+
 
 def basepage_scrape():
     """
-    DESCRIPTION: scrapes the homepage, gets all the
-                 links to country-specific pages
+    DESCRIPTION: scrapes the homepage, gets all the links to country pages
+                 and puts them in a dictionary that maps region to country links
     """
-    page = requests.get(baseurl)
+    page = requests.get(base_url)
     soup = BeautifulSoup(page.content, "html.parser")
     
-    # get the region list item
+    # get "the list" menu item from menubar
     region_title = soup.find("li", {"id": "pg383366003599184948"})
 
+    # find all ul's under "the list"
     region_country_ul = region_title.findChildren("ul")
 
-    regions = region_country_ul[0]
-    countries = region_country_ul[1:]
+    # separate ul list into just countries_uls list
+    countries_uls = region_country_ul[1:]
+
+    # store all country links in list of lists
+    countries_links = [ul.findChildren("a") for ul in countries_uls]
     
-    #region_links = [tag.findChildren("a")["href"] for tag in regions]
-    #countries_links = [tag.findChildren("a")["href"] for tag in countries]
+    # assemble region to country links dictionary mapping
+    region_country_links_dict = {
+        "South-America": countries_links[0],
+        "Central-America": countries_links[1],
+        "Asia": countries_links[2]
+    }
 
-    return regions
+    # scrape South-America country pages:
+    for country in region_country_links_dict["South-America"]:
+        country_link = country["href"]
+        country_page_scrape(country_link)
 
+    # store scraped data into csv
+    write_to_csv()
+
+    return region_country_links_dict["South-America"]
+
+
+def country_page_scrape(country_link):
+    """
+    DESCRIPTION: scrapes the country-specific ngo page
+    INPUT: country relative link scraped by basepage_scrape()
+    """
+    # assemble absolute URL for country-specific page
+    country_url = base_url + country_link
+
+    # get soup object
+    page = requests.get(country_url)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    # get the NGO table
+    ngo_tables = soup.find_all("table", class_="wsite-multicol-table")
+
+    # discard first two tables (general information about country)
+    ngo_tables = ngo_tables[2:]
+
+    for ngo_table in ngo_tables:
+        ngo_table_scrape(ngo_table)
+
+    return ngo_tables
+
+
+def ngo_table_scrape(ngo_table):
+    """
+    DESCRIPTION: scrapes one table of ngos on country-specific ngo page
+                 note: on this website, one table contains only one table row
+    INPUT: ngo table scraped from country-specific ngo page
+    """
+
+    # get all three tds in one ngo_table
+    tds = ngo_table.find_all("td")
+
+    # scrape ngo information from td
+    for td in tds:
+        divs = td.find_all("div")
+        # only scrape for valid td formats
+        if len(divs) > 3:
+            ngo_td_scrape(td)
+
+
+def ngo_td_scrape(td):
+    """
+    DESCRIPTION: scrapes one ngo td representing one ngo on country-specific ngo page
+    INPUT: ngo td scraped by ngo_table_scrape() on country-specific ngo page
+    """
+
+    divs = td.find_all("div")
+
+    # ngo name div corresponds to first div in td
+    ngo_name_div = divs[0]
+
+    # get ngo name from font tag under ngo name diiv
+    ngo_name = ngo_name_div.find_all("font")
+    if (len(ngo_name) > 0):
+        ngo_name = ngo_name[0].text
+
+    # ngo description div corresponds to 4th diiv in td
+    ngo_descr_div = divs[3]
+
+    print(ngo_descr_div.text)
 
 
 def write_to_csv():
@@ -49,8 +131,8 @@ def write_to_csv():
     DESCRIPTION: Writes everything stored in ngos list into a csv file
     """
     # write to csv to check output
-    keys = ["name", "description", "URL", "email"]
-    with open("thai_ngos.csv", "w") as w:
+    keys = ngos_store_keys
+    with open("ngo_list.csv", "w") as w:
         dict_writer = csv.DictWriter(w, keys)
         dict_writer.writeheader()
         dict_writer.writerows(ngos)
