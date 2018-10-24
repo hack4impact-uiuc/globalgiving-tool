59589590011c9b2a5bd2a60fdff7445ed931261d
Alan Ren <alanren2@illinois.edu>
2018-10-24 16:45:50 -0500

    works, besides it not printing to flask correctly

--- microservices/scraper-australia/scraper.py
+++ microservices/scraper-australia/scraper.py
@@ -5,44 +5,37 @@ import json
 website = "http://www.findouter.com/Oceania/Australia/Society-and-Culture/Non-Governmental-Organisations/"
 
 
-def helper(webpage, ret):
+def helper(webpage):
+    list_of_ds = []
     target_url = requests.get(webpage)
     page_data = BeautifulSoup(target_url.content, "html.parser")
     contents = page_data.find("div", {"class": "container"})
-    print(type(contents))
-    return contents
-    contents = contents.find_all("p")
-    for ngo in contents:
+    title = True
+    for ngo in str(contents).split('<div class="firstline">\n'):
         d = {}
-        ngo = str(ngo).split("<br/>\n")
-        roy_made_me_do_this(ngo, d)
-        ret.append(d)
-    for line in ngo:
-        if line[-4:] == "</p>":
-            line = line[:-4]
-        if line[:3] == "<p>":
-            d["Name"] = line[11:-9]
-        elif line[:7] == "P.O.Box":
-            d["P.O. Box"] = line[8:]
-        elif line[:4] == "www.":
-            d["Website"] = line
-        elif line[:8] == "Category":
-            d["Category"] = line[10:]
-        elif line[:16] == "Physical Address":
-            d["Physical Address"] = line[16:]
-        elif line[:9] == "Telephone":
-            d["Telephone"] = line[11:]
-        elif line[:6] == "E-mail":
-            d["E-mail"] = line[8:]
-        elif line[:14] == "Contact Person":
-            d["Contact Person"] = line[16:]
-        else:
-            d["Description"] = line
-
+        if title:
+            title = False
+            continue
+        # print(ngo)
+        for line in ngo.split('\n'):
+            if line[:23] == '<span class="sitename">':
+                line = line.split('>')
+                d["Website"] = line[1].split('"')[1]
+                d["Name"] = line[2][:-3]
+            elif line[:20] == '<span class="phone">':
+                d["Telephone"] = line[25:40]
+            elif line[:22] == '<span class="address">':
+                line = line.split('>')
+                d["Address"] = line[1][:-6]
+            elif line[:25] == '<div class="description">':
+                line = line.split('>')
+                d["Description"] = line[1][:-5]
+        list_of_ds.append(d)
+    return list_of_ds
 
 def get_page_data():
     ret = []
     for i in range(1,7):
         webpage = website + str(i)
-        return helper(webpage, ret)
+        ret.extend(helper(webpage))
     return json.dumps(ret)
