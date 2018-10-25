ca85978c8bd024f1c13389a7b76bc6d101ac257d
arshiam2 <arshiam2@illinois.edu>
2018-10-25 13:07:42 -0500

    cleaned up comments

--- microservices/scraper-ngothai/app/routes.py
+++ microservices/scraper-ngothai/app/routes.py
@@ -1,23 +1,3 @@
-# from flask import Flask
-# from app.scraper import basepage_scrape
-
-# app = Flask(__name__)
-
-
-# @app.route("/scrape_all")
-# def scrape_all_thai_ngos():
-#     # return str(basepage_scrape())
-#     return "Hello world!"
-
-
-# @app.route("/routes")
-# def return_all_routes():
-#     return ["/scrape_all"]
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 from app import app
 from app.scraper import basepage_scrape
 import json
