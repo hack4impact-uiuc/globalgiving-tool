1c722755f6efd23e0671036cfded1a026cce464f
Roy Chiu <rychiu13@gmail.com>
2018-10-31 18:38:45 -0500

    Implement templating with cookiecutter

--- microservices/scraper-{{cookiecutter.project_slug}}/app/routes.py
+++ microservices/scraper-{{cookiecutter.project_slug}}/app/routes.py
@@ -5,11 +5,17 @@ import json
 
 @app.route("/data")
 def page_data():
-    return scrape()
+    """
+    Gets the relevant data from the page
+    """
+    return
 
 
 @app.route("/routes")
-def routes_availible():
+def routes_available():
+    """
+    Returns a list of available routes to hit for this scraper.
+    """
     return json.dumps(
         ["%s" % rule for rule in app.url_map.iter_rules()],
         indent=4,
