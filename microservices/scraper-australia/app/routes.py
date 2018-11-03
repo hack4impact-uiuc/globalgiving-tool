2f355a6624d8096b20d98bcf9123d1f83fe64dd4
Alan Ren <alanren2@illinois.edu>
2018-11-03 17:57:46 -0500

    refactored

--- microservices/scraper-australia/app/routes.py
+++ microservices/scraper-australia/app/routes.py
@@ -1,7 +1,5 @@
-from flask import Flask
-from scraper import get_page_data
-
-app = Flask(__name__)
+from app import app
+from app.scraper import get_page_data
 
 
 @app.route("/routes")
@@ -12,7 +10,3 @@ def get_routes():
 @app.route("/data")
 def page_data():
     return str(get_page_data())
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
