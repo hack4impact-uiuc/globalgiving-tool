a11b33d81321ebdba8f2c0602376e09afa32564a
Skyler Shi <skylershi@Skylers-MacBook-Pro.local>
2018-11-03 23:24:21 -0500

    ngolist scraper done

--- microservices/scraper_ngolist/app.py
+++ microservices/scraper_ngolist/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from scraper import basepage_scrape
 from scraper import country_page_scrape
+import json
 
 app = Flask(__name__)
 
@@ -14,10 +15,39 @@ def scrape_all_ngos():
 def scrape_colombia():
     return str(country_page_scrape("/colombia.html"))
 
+@app.route("/ecuador")
+def scrape_ecuador():
+    return str(country_page_scrape("/ecuador.html"))
+
+@app.route("/peru")
+def scrape_peru():
+    return str(country_page_scrape("/peru.html"))
+
+@app.route("/bolivia")
+def scrape_bolivia():
+    return str(country_page_scrape("/bolivia.html"))
+
+@app.route("/argentina")
+def scrape_argentina():
+    return str(country_page_scrape("/argentina.html"))
+
+@app.route("/chile")
+def scrape_chile():
+    return str(country_page_scrape("/chile.html"))
+
+@app.route("/brazil")
+def scrape_brazili():
+    return str(country_page_scrape("/brazili.html"))
+
 
 @app.route("/routes")
-def return_all_routes():
-    return ["/scrape_all", ]
+def routes_available():
+    """Returns a list of available routes to hit for this scraper."""
+    return json.dumps(
+        ["%s" % rule for rule in app.url_map.iter_rules()],
+        indent=4,
+        separators=(",", ": "),
+    )
 
 
 if __name__ == "__main__":
--- microservices/scraper_ngolist/scraper.py
+++ microservices/scraper_ngolist/scraper.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 from urllib.request import urlopen
 import csv
+import re
 
 base_url = "https://www.thengolist.com"
 
@@ -10,14 +11,11 @@ base_url = "https://www.thengolist.com"
 ngos = []
 ngos_store_keys = [
     "ngo_name",
-    "ngo_URL",
-    "phone_num",
-    "email",
-    "registration_id",
-    "year_established",
     "description",
+    "ngo_URL",
+    "facebook",
+    "email",
     "country"
-    "facebook"
 ]
 
 #dictionary of key:value pairs of "region": [list of country links]
@@ -34,13 +32,10 @@ def basepage_scrape():
     
     # get "the list" menu item from menubar
     region_title = soup.find("li", {"id": "pg383366003599184948"})
-
     # find all ul's under "the list"
     region_country_ul = region_title.findChildren("ul")
-
     # separate ul list into just countries_uls list
     countries_uls = region_country_ul[1:]
-
     # store all country links in list of lists
     countries_links = [ul.findChildren("a") for ul in countries_uls]
     
@@ -51,7 +46,8 @@ def basepage_scrape():
         "Asia": countries_links[2]
     }
 
-    # scrape South-America country pages:
+    # scrape only South-America country pages:
+    # other continent country pages do not contain enough useful information
     for country in region_country_links_dict["South-America"]:
         country_link = country["href"]
         country_page_scrape(country_link)
@@ -65,7 +61,7 @@ def basepage_scrape():
 def country_page_scrape(country_link):
     """
     DESCRIPTION: scrapes the country-specific ngo page
-    INPUT: country relative link scraped by basepage_scrape()
+    INPUT: country_link - country relative link scraped by basepage_scrape()
     """
     # assemble absolute URL for country-specific page
     country_url = base_url + country_link
@@ -81,12 +77,13 @@ def country_page_scrape(country_link):
     ngo_tables = ngo_tables[2:]
 
     for ngo_table in ngo_tables:
-        ngo_table_scrape(ngo_table)
+        # remove ".html" at end of link & "/" at beginning of link to get country name
+        ngo_table_scrape(ngo_table, country_link[:-5][1:])
 
     return ngo_tables
 
 
-def ngo_table_scrape(ngo_table):
+def ngo_table_scrape(ngo_table, country_name):
     """
     DESCRIPTION: scrapes one table of ngos on country-specific ngo page
                  note: on this website, one table contains only one table row
@@ -101,36 +98,90 @@ def ngo_table_scrape(ngo_table):
         divs = td.find_all("div")
         # only scrape for valid td formats
         if len(divs) > 5:
-            ngo_td_scrape(td)
+            ngo_td_scrape(td, country_name)
 
 
-def ngo_td_scrape(td):
+def ngo_td_scrape(td, country_name):
     """
     DESCRIPTION: scrapes one ngo td representing one ngo on country-specific ngo page
     INPUT: ngo td scraped by ngo_table_scrape() on country-specific ngo page
     """
-    ngo_name_div, ngo_descr_div, ngo_contact_div = td.find_all("div", {"class": "paragraph"})
+    # intialize all NGO information entresi
+    ngo_name = ""
+    ngo_descr = ""
+    ngo_URL = ""
+    ngo_facebook = ""
+    ngo_email = ""
+    ngo_country = country_name
 
+    try:
+        ngo_name_div, ngo_descr_div, ngo_contact_div = td.find_all("div", {"class": "paragraph"})
+    except ValueError:
+        print("Country could not be scraped.")
+        return
+    # -----------------------FIND NGO NAME-----------------------------
+    ngo_name_font_tags = ngo_name_div.find_all("font", {"size":"5"})
+    # ngo name's font tag either has no children or is
+    # is the first child of the font tags found
+    # CASE 1:
+    # <font color = "#000">
+    #     <font size = "5"> NAME </font>
+    # </font>
+    # CASE 2:
+    # <font size = "5">
+    #     <font color = "#000"> NAME </font>
+    #     <font> DONT CARE </font>
+    # </font>
+    for font_tag in ngo_name_font_tags:
+        if (font_tag.findChild() == None):
+            ngo_name = font_tag.text
+            break
+        else:
+            ngo_name = font_tag.findChild().text
+            break
 
-    #take out strong tags text in ngo name
-    # for i, strong in enumerate(ngo_name_div('strong')):
-    #     if i != 0:
-    #         strong.decompose()
-    ngo_name_font_tags = ngo_name_div.find_all("font")
-    ngo_name = ngo_name_font_tags[1].text
+    if(ngo_name == ""):
+        print("Ngo name was not found!!")
 
-    #take out strong tags text in ngo description
+    # -----------------------FIND NGO DESCRIPTION---------------------------
     for strong in ngo_descr_div('strong'):
         strong.decompose()
     ngo_descr = ngo_descr_div.get_text()
 
+
+    # -----------------------FIND NGO CONTACT INFO-----------------------------
     ngo_contact_a_tags = ngo_contact_div.find_all("a")
+    if ngo_contact_a_tags != None:
+        try:
+            ngo_URL = ngo_contact_a_tags[0]['href']
+        except IndexError:
+            print("ngo URL could not be found!")
+        try:
+            ngo_facebook = ngo_contact_a_tags[1]['href']
+        except IndexError:
+            print("ngo facebook could not be found!")
+        
 
-    ngo_URL = ngo_contact_a_tags[0]['href']
-    ngo_facebook = ngo_contact_a_tags[1]['href']
+    ngo_contact_text = ngo_contact_div.text
+    ngo_contact_text = ngo_contact_text.replace("fb:", " ")
+    match = re.search(r'mail:[\w\. -]+\(at\)[\w\. -]+\(dot\)[\w\. -]+', ngo_contact_text)
+    
+    if(match == None):
+        print("NGO email could not be found!!")
+    else:
+        ngo_email = email_format(match.group())
 
-    print(ngo_URL)
-    print(ngo_facebook)
+    # assemble ngo dict
+    ngo = {}
+    ngo["ngo_name"] = ngo_name
+    ngo["description"] = ngo_descr
+    ngo["ngo_URL"] = ngo_URL
+    ngo["facebook"] = ngo_facebook
+    ngo["email"] = ngo_email
+    ngo["country"] = ngo_country
+    print(ngo)
+    # append ngo dictionary to list of ngos
+    ngos.append(ngo)
 
 
 def email_format(email_string):
@@ -139,8 +190,12 @@ def email_format(email_string):
     INPUT: email string denoting email read from webpage
     OUTPUT: properly formatted email string ready for store
     """
+    ngo_email = email_string.replace("mail:", "")
+    ngo_email = ngo_email.replace(" (at) ", "@")
+    ngo_email = ngo_email.replace(" (dot) ", ".")
+    ngo_email = ngo_email.lstrip().rstrip()
 
-    return ""
+    return ngo_email
 
 
 def write_to_csv():
