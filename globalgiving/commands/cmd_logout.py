391665b4cc2f0f0dcbf62b34cf27253b350d6b64
Aria Malkani <arshiam2828@gmail.com>
2018-11-30 12:52:44 -0600

    whoami & logout commands (#71)
    * whoami command
    
    * added a logout functionality
    
    * linting:

---
+++ globalgiving/commands/cmd_logout.py
@@ -0,0 +1,14 @@
+import os
+import click
+import jwt
+from globalgiving.cli import pass_context
+
+
+@click.command("logout", short_help="Registers a new user")
+@pass_context
+def cli(ctx):
+    if os.path.exists(".jwt"):
+        os.remove(".jwt")
+        print("You are logged out")
+    else:
+        print("The file does not exist")
