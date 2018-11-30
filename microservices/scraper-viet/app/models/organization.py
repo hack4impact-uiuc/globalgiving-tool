fb0b49c842546beecc8e36369e3375624abf09b4
Aria Malkani <arshiam2828@gmail.com>
2018-11-30 06:53:38 -0600

    Viet Scraper (#68)
    * updated viet scraper
    
    * black
    
    * Updated tests

--- microservices/scraper-viet/app/models/organization.py
+++ microservices/scraper-viet/app/models/organization.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Org:
     """
     Serialized object for easy input and retrieval of relevant organization text data.
@@ -5,13 +8,29 @@ class Org:
     and web url.
     """
 
-    def __init__(self, name, phone, email, address, contact, url):
+    def __init__(
+        self=None,
+        name=None,
+        phone=None,
+        email=None,
+        address=None,
+        contact=None,
+        registration=None,
+        url=None,
+        facebook=None,
+        country=None,
+        description=None,
+    ):
         self.name = name
         self.phone = phone
         self.email = email
         self.address = address
         self.contact = contact
+        self.registration = registration
         self.url = url
+        self.facebook = facebook
+        self.country = country
+        self.description = description
 
     def to_json(self):
         org = {
@@ -20,16 +39,13 @@ class Org:
             "email": self.email,
             "address": self.address,
             "contact": self.contact,
+            "registration": self.registration,
             "url": self.url,
+            "facebook": self.facebook,
+            "country": self.country,
+            "description": self.description,
         }
         return org
 
     def __str__(self):
-        return "%s, %s, %s, %s, %s, %s" % (
-            self.name,
-            self.phone,
-            self.email,
-            self.address,
-            self.contact,
-            self.url,
-        )
+        return json.dumps(self.to_json(), indent=4, separators=(",", ": "))
