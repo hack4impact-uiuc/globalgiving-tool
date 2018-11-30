5b38c6b1d3fca47b67766bf0c79315ee1ee73229
Aryn Harmon <achcello@gmail.com>
2018-11-30 11:00:24 -0600

    update run with db uploading stuff

--- globalgiving/commands/cmd_run.py
+++ globalgiving/commands/cmd_run.py
@@ -1,58 +1,24 @@
-import click, requests
-
-import hashlib
-import os
-import uuid
-from globalgiving.db import list_from_db
-from globalgiving.cli import pass_context
-from ..s3_interface import *
+import click
+import requests
+from gg.db import list_from_db, upload_data
+from gg.cli import pass_context
 
 
 @click.command("run", short_help="Run a scraper")
-@click.argument("n", required=True)
+@click.argument("name", required=True, type=str)
 @pass_context
-def cli(ctx, n):
-    client = init_s3_credentials()
-
-    h = hashlib.md5()
-    h.update(n.encode("utf-8"))
-    bucket_name = n + "-" + h.hexdigest()
-
-    filename = str(uuid.uuid4()) + ".txt"
-    f = open(filename, "w+")
-
+def cli(ctx, name):
     search = "Finding scraper {} from list of registered scrapers..."
-    f.write(search.format(n) + "\n")
+    ctx.log(search.format(name))
     try:
         scrapers = list_from_db()
-        route = list(filter(lambda scraper: scraper["name"] == str(n), scrapers))[0][
+        route = list(filter(lambda scraper: scraper["name"] == str(name), scrapers))[0][
             "routes"
         ]["Data"]
-        f.write("Scraper {} found!".format(n) + "\n")
+        ctx.log("Scraper {} found!".format(name))
     except StopIteration:
-        f.write("Scraper {} not found!".format(n) + "\n")
-        f.close()
-        client.upload_file(filename, bucket_name, filename)
-        os.remove(filename)
+        ctx.log("Scraper {} not found.".format(name))
         return
-    try:
-        contents = requests.get(route).text + "\nSUCCESS"
-    except Exception as e:
-        contents = str(e) + "\nFAILED"
-
-    f.write(contents)
-    f.close()
-
-    # Call S3 to list current buckets
-    response = client.list_buckets()
-
-    # Get a list of all bucket names from the response
-    buckets = [bucket['Name'] for bucket in response['Buckets']]
-
-    if not (bucket_name in buckets):
-        client.create_bucket(Bucket=bucket_name)
-
-    client.upload_file(filename, bucket_name, filename)
-
-    os.remove(filename)
-    ctx.log("Wrote logs to file: " + filename)
+    contents = requests.get(route).json()
+    result = upload_data(contents)
+    ctx.log(result)
