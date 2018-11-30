cc843d91a1663f1ffea9b38a7cbaeab17155e321
arshiam2 <arshiam2@illinois.edu>
2018-11-30 12:37:57 -0600

    whoami command

---
+++ globalgiving/commands/cmd_whoami.py
@@ -0,0 +1,16 @@
+import os
+import click
+import jwt
+from globalgiving.cli import pass_context
+
+@click.command("whoami", short_help="Registers a new user")
+@pass_context
+def cli(ctx):
+    try:
+        with open(".jwt", "rb") as f:
+            byte = f.read()
+            decoded = jwt.decode(byte, 'secret', algorithms=['HS256'])
+            print("You are logged in as " + decoded["user"])
+    except:
+        print("You are not currently logged in")
+    
