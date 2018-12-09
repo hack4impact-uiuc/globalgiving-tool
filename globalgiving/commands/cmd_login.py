e7d7139801f1ade5393ca1887c4993fede400253
Roy Chiu <rychiu13@gmail.com>
2018-12-09 04:45:37 -0600

    Updated command descriptions

--- globalgiving/commands/cmd_login.py
+++ globalgiving/commands/cmd_login.py
@@ -5,7 +5,7 @@ import dotenv
 from globalgiving.cli import pass_context
 
 
-@click.command("register-user", short_help="Registers a new user")
+@click.command("register-user", short_help="Login a returning user")
 @click.option("--username", prompt=True)
 @click.option("--password", prompt=True, hide_input=True)
 @pass_context
