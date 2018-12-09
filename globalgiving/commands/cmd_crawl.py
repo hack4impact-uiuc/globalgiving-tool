3e3aeac9d5bd979d9fc6d6c25e7f43eced5afe4b
arshiam2 <arshiam2@illinois.edu>
2018-12-09 07:23:20 -0600

    crawl functions cleanup

--- globalgiving/commands/cmd_crawl.py
+++ globalgiving/commands/cmd_crawl.py
@@ -3,22 +3,19 @@ import requests
 from googlesearch import search
 from globalgiving.cli import pass_context
 from globalgiving.crawl_functions import rank_all, url_rank
-
+from urllib.parse import urlparse
 
 @click.command("crawl", short_help="Crawl for new directories and NGOs")
 @click.argument("country", required=True)
-@click.argument("ngo_type", required=True)
 @pass_context
-def cli(ctx, country, ngo_type):
+def cli(ctx, country):
     urls = []
-    for url in search("ngo directory" + country + ngo_type, lang="es", num=5, stop=1):
-        # print(url)
-        url_rank[url] = []
-    rank_all(country, ngo_type)
-#     for url in search("ngo directory" + country + ngo_type, lang="es", num=5, stop=1):
-#         print(url)
-#         url_rank[url] = []
-#         rank_all(country, ngo_type)
-   
+    for url in search("ngo directory" + country , lang="es", num=5, stop=1):
+        parsed_uri = urlparse(url)
+        home_url = "{uri.scheme}://{uri.netloc}/".format(uri=parsed_uri)
+        if (str(home_url) not in url_rank):
+            print("Added url " + str(home_url))
+            url_rank[home_url] = []
+    rank_all(country)
     
     print(url_rank)
--- globalgiving/crawl_functions.py
+++ globalgiving/crawl_functions.py
@@ -3,10 +3,9 @@ from bs4 import BeautifulSoup
 from bs4.element import Comment
 from urllib.parse import urlparse
 import requests
-import phonenumbers
 from globalgiving.resources.address_keywords import address_keywords
 from globalgiving.resources.country_codes import country_codes
-
+from globalgiving.rank import count_phone_numbers,count_addresses,count_ngo_related_words,get_composite_score
 
 # dictionary that maps directory url to rank_info for one website
 url_rank = {}
@@ -20,7 +19,7 @@ rank_info = {}
     'num_subpages': (number of subpages)
     'num_addresses': (number of addresses)
     'num_phone_numbers': (number of phone numbers)
-    'num_word_ngo' : (number of times word "ngo" appears on website)
+    'num_word_ngo' : (number of times word "ngo", "directory", or "nonprofit" appears on website)
     'num_word_directory': (number of times word "directory appears on website)
     'composite_score': (composite score for website)
     'page_or_dir': (boolean to indicate whether we think URL is ngo page or ngo directory)
@@ -30,13 +29,12 @@ country_name = ""
 ngo_type_name = ""
 
 
-def rank_all(country, ngo_type):
+def rank_all(country):
     """
     Goes through all the urls in the url_rank dictionaryn and ranks them 
     Returns a dictionary of ngo directory url mapped to ranking information
     """
     country_name = country.title()
-    ngo_type_name = ngo_type
     for url, _ in url_rank.items():
         rank_page(url)
     return url_rank
@@ -67,99 +65,22 @@ def rank_page(url):
 
     # perform webpage analysis on all_visible_text HERE, update rank_info
     rank_info["url"] = url
-    rank_info["num_phone_numbers"] = count_phone_numbers(all_visible_text)
+    rank_info["num_phone_numbers"] = count_phone_numbers(country_name, all_visible_text)
     rank_info["num_addresses"] = count_addresses(all_visible_text)
+    rank_info["num_subpages"] = str(len(subpages))
+    rank_info["num_word_ngo"] = str(count_ngo_related_words(all_visible_text))
+    rank_info["composite_score"] = str(get_composite_score(rank_info))
+
+    # count_ngos
     
     print("     Has " +  str(rank_info["num_phone_numbers"]) + " phone numbers")
     print("     Has " +  str(rank_info["num_addresses"]) + " addresses")
+    print("     Has " +  str(rank_info["num_subpages"]) + " subpages")
+    print("     Has " +  str(rank_info["num_word_ngo"]) + " appearances of ngo directory related words")
+    print("     Composite Score " +  rank_info["composite_score"])
 
-    # get composite_score
-    print("    Composite Score " +  str(get_composite_score(rank_info)))
-    url_rank[url]['composite_score'] = composite_score
-
-
-
-def count_phone_numbers(visible_text):
-    """
-    DESCRIPTION: counts number of phone numbers that occur on NGO website
-    INPUT: visible_text --- all the visible text of NGO website homepage and subpages
-    OUTPUT: integer number of phone numbers found in visible text
-    """
-    if country_name not in country_codes:
-        code = "None"
-    else:
-        code = country_codes[country_name]
-        print(code)
-    num_phone_numbers = 0
-    for match in phonenumbers.PhoneNumberMatcher(visible_text, code):
-        num_phone_numbers += 1
-
-    return num_phone_numbers
-
-
-def count_addresses(visible_text):
-    """
-    DESCRIPTION: counts number of addresses that occur on NGO website
-    INPUT: visible_text --- all the visible text of NGO website homepage and subpages
-    OUTPUT: integer number of addresses found in visible text
-    """
-    visible_text = visible_text.lower()
-    num_addresses = 0
-
-    for type_ in address_keywords:
-        num_addresses += visible_text.count(type_)
-
-    return num_addresses
-
-
-def count_ngos(visible_text):
-    """
-    DESCRIPTION: counts number of instances of 'ngo'
-    INPUT: visible_text --- all the visible text of NGO website homepage and subpages
-    OUTPUT: integer number of instances of 'ngo'
-    """
-    visible_text = visible_text.lower()
-    return visible_text.count("ngo")
-
-
-def count_directories(visible_text):
-    """
-    DESCRIPTION: counts number of instances of 'directory'
-    INPUT: visible_text --- all the visible text of NGO website homepage and subpages
-    OUTPUT: integer number of instances of 'directory'
-    """
-    visible_text = visible_text.lower()
-    return visible_text.count("directory")
-
-
-def get_composite_score(rank_info):
-    """
-    DESCRIPTION: ranks the NGO website specified by url
-    INPUT: url --- URL to NGO website
-    OUTPUT: none
-    BEHAVIOR: expect url_rank to now contain dictionary of rank_info as value. This
-              dictionary stores information pertinent to ranking as well as composite rank score
-    """
-    # heuristic can be altered here:
-    # composite_score = rank_info['num_phone_numbers']
-    composite_score = rank_info["num_phone_numbers"] + \
-        rank_info["num_addresses"]
-    rank_info["composite_score"] = composite_score
-    url = rank_info["url"]
+    # print("    Composite Score " +  str(get_composite_score(rank_info)))
     url_rank[url] = rank_info
-    # return composite_score
-    return rank_info["composite_score"]
-
-
-# -----------------------------------------------------------------------------------------------
-# -----------------------------------RANKING FUNCTIONS-------------------------------------------
-# -----------------------------------------------------------------------------------------------
-
-
-# -----------------------------------------------------------------------------------------------
-# ------------------------------FUNCTIONS TO GET VISIBLE TEXT-------------s----------------------
-# -----------------------------------------------------------------------------------------------
-
 
 def get_all_visible_text(url):
     """
@@ -206,12 +127,7 @@ def find_subpages(url):
     INPUT: url --- absolute URL denoting ngo website homepage
     OUTPUT: list of all subpage URLs
     """
-
-    # get the domain url from homepage url
-    parsed_uri = urlparse(url)
-    home_url = "{uri.scheme}://{uri.netloc}/".format(uri=parsed_uri)
-    # print("STRIPPED URL:")
-    print("Fecthing subpages for " + str(home_url))
+    print("Fecthing subpages for " + url)
 
     subpages = []
     valid_subpages = []
@@ -232,27 +148,26 @@ def find_subpages(url):
     2) absolute links for subpages (e.g. https://care.ca/about-us/mission)
     3) irrevelant external absolute link (e.g. https://mcafee.com/blahblahblah)
     """
-    home_url_length = len(home_url)
+    # home_url_length = len(home_url)
     # consider case 1) and 2) for subpage links, discard case 3)
     for link in links:
         # discard case 3)
-        if link[:1] != "/" and link[:home_url_length] != home_url:
+        if link[:1] != "/" and link[:len(url)] != url:
             # print("LINK IS NOT SUBPAGE: " + link)
             continue
         # case 2)
-        if link[:home_url_length] == home_url:
+        if link[:len(url)] == url:
             subpages.append(link)
         # case 1)
         if link[:1] == "/":
             subpages.append(url + link)
-            subpages.append(home_url + link)
+            subpages.append(url + link)
 
     # remove duplicates in valid_subpages
     subpages = list(set(subpages))
 
     # remove faulty subpage links
     for link in subpages:
-        # print(link)
         # try to access subpage link
         try:
             r = requests.get(str(link))
@@ -262,19 +177,5 @@ def find_subpages(url):
             break
         except:
             continue
-
-    # print("VALID SUBPAGES: " + str(len(valid_subpages)))
-    # print(valid_subpages)
-
-    # # output all subpage links to text file for easier examination
-    # f = open("subpages.txt", "w")
-    # for valid_subpage in valid_subpages:
-    #     f.write(valid_subpage + "\n")
-    # f.close()
-
     return valid_subpages
 
-
-# -----------------------------------------------------------------------------------------------
-# ------------------------------FUNCTIONS TO GET VISIBLE TEXT-------------s----------------------
-# -----------------------------------------------------------------------------------------------
