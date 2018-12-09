f8a428adcc296624aa7bc3542451359961a3e5e6
Roy Chiu <rychiu13@gmail.com>
2018-12-09 05:27:09 -0600

    edited/updated crawler

--- globalgiving/commands/cmd_crawl.py
+++ globalgiving/commands/cmd_crawl.py
@@ -1,7 +1,7 @@
 import click
 import requests
-from globalgiving.cli import pass_context
 from googlesearch import search
+from globalgiving.cli import pass_context
 from globalgiving.crawl_functions import rank_all, url_rank
 
 
@@ -11,6 +11,6 @@ from globalgiving.crawl_functions import rank_all, url_rank
 @pass_context
 def cli(ctx, country, ngo_type):
     for url in search("ngo directory" + country + ngo_type, lang="es", num=1, stop=1):
-        print(url)
+        print("Currently searching: {}".format(url))
         url_rank[url] = []
-        rank_all(country,ngo_type)
+        rank_all(country, ngo_type)
