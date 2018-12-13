0c53d77d2ea28c9dffb996a4247715eea736700f
Roy Chiu <rychiu13@gmail.com>
2018-12-12 20:05:49 -0600

    lint

--- microservices/tests/scraper_hati_malaysia_test.py
+++ microservices/tests/scraper_hati_malaysia_test.py
@@ -1,5 +1,6 @@
 import pytest
 from scraper_hati_malaysia.app import scraper
 
+
 def test_basic():
     assert scraper.scrape(one=True) is not None
--- microservices/tests/scraper_ngouganda_test.py
+++ microservices/tests/scraper_ngouganda_test.py
@@ -1,5 +1,6 @@
 import pytest
 from scraper_ngouganda.app import scraper
 
+
 def test_basic():
     assert scraper.get_one() is not None
