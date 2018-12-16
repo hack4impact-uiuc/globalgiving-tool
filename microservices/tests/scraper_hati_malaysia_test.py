62fef7efcc4b83e91cf071160218f764fe503ddf
Aryn Harmon <achcello@gmail.com>
2018-12-16 13:37:39 -0600

    add test for malaysia

--- microservices/tests/scraper_hati_malaysia_test.py
+++ microservices/tests/scraper_hati_malaysia_test.py
@@ -1,6 +1,27 @@
 import pytest
-from scraper_hati_malaysia.app import scraper
+from scraper_hati_malaysia.app import scraper, get_cat_links, get_ngo_links, get_ngo_information
 
 
 def test_basic():
     assert scraper.scrape(one=True) is not None
+
+
+def test_sections():
+    # get the categories
+    categories = get_cat_links()
+    number_of_categories = 18  # from hati.my
+    # if this test fails, then either the scraper has failed or the source
+    # website has changed.
+    assert len(categories) == number_of_categories
+
+    # get the links associated with a couple categories
+    links = get_ngo_links(categories[:2])
+    # we will use an approximate number for this test as the categories can
+    # sometimes appear in different orders
+    minimum_links = 3
+    assert len(links) > minimum_links
+
+    # check if the links are valid by getting a couple sets of information
+    information = get_ngo_information(links[:2])
+    for ngo in information:
+        assert ngo is not None
