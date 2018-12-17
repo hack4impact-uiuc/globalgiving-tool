49c5f83300fc1c5c6da192f83e86b0e5f39cdc4e
arshiam2 <arshiam2@illinois.edu>
2018-12-17 08:44:59 -0600

    test fixed?

--- microservices/tests/scraper_india_test.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from microservices.scraper_india.app import scraper
-
-
-def test_basic():
-    assert scraper.get_one_nonprofit() is not None
--- microservices/tests/scraper_ngolist_test.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from microservices.scraper_ngolist.app import scraper
-
-
-def test_basic():
-    assert scraper.basepage_scrape() is not None
