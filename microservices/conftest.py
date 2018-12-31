a6da5fafa5c2f24b87a9ff4182341bf31f4b357c
Roy Chiu <rychiu13@gmail.com>
2018-12-31 00:09:30 -0600

    lint

--- microservices/conftest.py
+++ microservices/conftest.py
@@ -1,9 +1,11 @@
 import pytest
 import sys, os
 
+
 def pytest_sessionstart(session):
     # Add microservice directory to path for test case imports to work
     sys.path.append(os.path.realpath(os.path.dirname(__file__)))
+
 
 def pytest_sessionfinish(session, exitstatus):
     # Cleanup, return sys.path to original state
