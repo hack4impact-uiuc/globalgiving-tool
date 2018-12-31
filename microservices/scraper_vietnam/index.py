f7ba2619d0884b3e4c26c242e6176a378fe1ce29
arshiam2 <arshiam2@illinois.edu>
2018-12-30 23:39:27 -0800

    udpate

--- microservices/scraper_vietnam/index.py
+++ microservices/scraper_vietnam/index.py
@@ -1,32 +1,26 @@
 from flask import Flask
 from flask_restplus import Resource, Api, fields
-from werkzeug.contrib.fixers import ProxyFix
 from scraper import get_test_data, get_page_data
 
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app)
 api = Api(app, version="0.1", title="Vietnam Scraper")
-
 
 @api.route("/url")
 class ScraperVietnam(Resource):
     def get(self):
         return {"url": "https://www.viet.net/community/nonprofit/"}
 
-
 @api.route("/data")
-class ScraperVietnam2(Resource):
+class ScraperVietnam(Resource):
     def get(self):
         orgs = get_page_data()
         return {"data": orgs}
 
-
 @api.route("/test")
-class ScraperVietnam3(Resource):
+class ScraperVietnam(Resource):
     def get(self):
         orgs = get_test_data()
         return {"test": str(orgs[0])}
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- microservices/scraper_vietnam/tests/test_vietnam.py
+++ microservices/scraper_vietnam/tests/test_vietnam.py
@@ -1,12 +1,5 @@
-import sys, os
-
-sys.path.append(os.path.realpath(os.path.dirname(__file__) + "/.."))
-
-from scraper import get_page_data
-
+from .scraper import get_page_data
 
 def test():
     assert len(get_page_data()) >= 1
 
-
-test()
