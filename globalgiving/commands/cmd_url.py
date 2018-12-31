cc01f5b06818355293dc85a2382eabb16d8c1c58
arshiam2 <arshiam2@illinois.edu>
2018-12-31 14:57:04 -0800

    udpate

--- globalgiving/commands/cmd_url.py
+++ globalgiving/commands/cmd_url.py
@@ -4,6 +4,7 @@ from globalgiving.db import list_scrapers_from_db
 from globalgiving.cli import pass_context, authenticate
 import json
 
+
 @click.command("url", short_help="Lists all the urls with scrapers")
 @click.option(
     "--scraper_name",
@@ -18,7 +19,7 @@ def cli(ctx, scraper_name):
     if scraper_name:
         for scraper in scrapers:
             if scraper["name"] == scraper_name:
-                url = requests.get("http://" + scraper["_id"] +"/url").text
+                url = requests.get("http://" + scraper["_id"] + "/url").text
                 if "http" in url:
                     print(url)
                     return
@@ -26,7 +27,7 @@ def cli(ctx, scraper_name):
     for scraper in scrapers:
         print("Scraper: " + scraper["name"])
         # contents = requests.get(scraper["routes"]["Test"][0:-4] + "url").text
-        contents = requests.get("http://" + scraper["_id"] +"/url").text
+        contents = requests.get("http://" + scraper["_id"] + "/url").text
         if "http" in contents:
             print("       " + contents)
         else:
