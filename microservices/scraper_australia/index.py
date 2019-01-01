ce20f98bbc22b35da3ad6ca8becb947772682a87
arshiam2 <arshiam2@illinois.edu>
2019-01-01 01:18:37 -0800

    austraia updated:

---
+++ microservices/scraper_australia/index.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_restplus import Resource, Api, fields
+from werkzeug.contrib.fixers import ProxyFix
+from src.scraper import get_one_nonprofit, get_page_data
+
+app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
+api = Api(app, version="0.1", title="Vietnam Scraper")
+
+
+@api.route("/url")
+class ScraperVietnam(Resource):
+    def get(self):
+        return "http://www.findouter.com/Oceania/Australia/Society-and-Culture/Non-Governmental-Organisations/"
+
+
+@api.route("/data")
+class ScraperVietnam(Resource):
+    def get(self):
+        orgs = get_page_data()
+        return {"data": orgs}
+
+
+@api.route("/test")
+class ScraperVietnam(Resource):
+    def get(self):
+        return {"test": get_one_nonprofit()}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
