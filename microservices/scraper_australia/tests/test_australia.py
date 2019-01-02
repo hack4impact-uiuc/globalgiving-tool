b9e7316d00dc6d4a04f80c625864c37ea39d37be
arshiam2 <arshiam2@illinois.edu>
2019-01-02 01:03:11 -0800

    added tests for australia scraper

--- microservices/scraper_australia/tests/test_australia.py
+++ microservices/scraper_australia/tests/test_australia.py
@@ -1,5 +1,16 @@
-from scraper_vietnam.src.scraper import get_page_data
+from scraper_australia.src.scraper import get_page_data, parse_page, get_one_nonprofit
 
 
-def test():
+def testGetPageData():
     assert len(get_page_data()) >= 1
+
+
+def testgetOne():
+    get_one_nonprofit()
+    assert (get_one_nonprofit())["country"] == "Australia"
+    assert (get_one_nonprofit())["name"] == "CARE Australia"
+
+
+def testParsePage():
+    webpage = "http://www.findouter.com/Oceania/Australia/Society-and-Culture/Non-Governmental-Organisations/1"
+    assert len(parse_page(webpage)) >= 1
