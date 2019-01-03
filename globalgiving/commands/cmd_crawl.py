15abfeba3ea60b60bd3f7e73276b9b20a4100dcf
Roy Chiu <rychiu13@gmail.com>
2019-01-03 12:59:07 -0600

    Deleted comments in cli tool, small refactor

--- globalgiving/commands/cmd_crawl.py
+++ globalgiving/commands/cmd_crawl.py
@@ -33,7 +33,6 @@ def cli(ctx, country, number_urls):
         home_url = "{uri.scheme}://{uri.netloc}/".format(uri=parsed_uri)
         print("Crawling --- ", home_url)
         if str(home_url) not in url_rank:
-            #     for url in url_rank:
             # Check if url has already been ranked before
             cursor = ranked_link.find({"url": home_url})
             document_list = [url for url in cursor]
@@ -62,7 +61,6 @@ def dev_crawl(collection, country, number_urls=1):
         parsed_uri = urlparse(url)
         home_url = "{uri.scheme}://{uri.netloc}/".format(uri=parsed_uri)
         if str(home_url) not in url_rank:
-            #     for url in url_rank:
             cursor = collection.find({"url": home_url})
             document_list = [url for url in cursor]
             if len(document_list) == 0:
--- globalgiving/s3_interface.py
+++ globalgiving/s3_interface.py
@@ -3,12 +3,14 @@ import boto3
 import os
 import json
 import pymongo
+from globalgiving.db import db_get_collection
 from globalgiving.config import (
     CREDENTIALS_PATH,
     CRED_URI_FIELD,
     CRED_TOKEN_FIELD,
     CRED_ACCESS_FIELD,
     CRED_SECRET_FIELD,
+    CRED_COLLECTION,
 )
 
 
@@ -16,16 +18,8 @@ def init_s3_credentials():
 
     with open(os.getenv("HOME") + CREDENTIALS_PATH) as f:
         data = json.load(f)
-
-        # print(os.getenv("HOME") + "/globalgiving/credentials.json")
-        # print(data)
-        # mongo_uri = os.getenv(data["mongo_uri"])
-        # print(mongo_uri)
-        client = pymongo.MongoClient(data[CRED_URI_FIELD])
-
-        db = client.get_database()
-
-        user_information = db["credentials"].find_one(
+        collection = db_get_collection(CRED_COLLECTION)
+        user_information = collection.find_one(
             {
                 CRED_URI_FIELD: data[CRED_URI_FIELD],
                 CRED_TOKEN_FIELD: data[CRED_TOKEN_FIELD],
@@ -43,6 +37,5 @@ def init_s3_credentials():
             "s3",
             aws_access_key_id=access_key,
             aws_secret_access_key=secret_key,
-            # aws_session_token=SESSION_TOKEN,
         )
         return client
